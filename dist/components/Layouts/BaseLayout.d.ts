69f64d8ef95365e1a825f5490d91486100c66562
ibfree <97336943+ibfree@users.noreply.github.com>
2022-01-08 14:17:55 +0100

    Update BaseLayout.d.ts

--- dist/components/Layouts/BaseLayout.d.ts
+++ dist/components/Layouts/BaseLayout.d.ts
@@ -1,2 +1,6 @@
 declare const GridLayout: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
 export default GridLayout;
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@pancakeswap-libs/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
